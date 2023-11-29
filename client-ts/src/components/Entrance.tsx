ac40c0239779e403288bcd76f815cb4ee2615ae5
Alexander Lead <a.lead@yandex.ru>
2023-11-29 21:39:17 +0300

    upd(front): screeshot

--- client-ts/src/components/Entrance.tsx
+++ client-ts/src/components/Entrance.tsx
@@ -15,7 +15,7 @@ const Entrance = ({username, setUsername, room, setRoom, joinRoom }: EntranceObj
   return (
     <div className="layout">
         <div className="enterance">
-        <div className="enterance__header"> <h3>Enterance</h3></div>
+        <div className="enterance__header"> <h3>Entrance</h3></div>
         <div className="enterance__body">
         <input type="text" placeholder='Enter your name' onChange={(e)=>setUsername(e.target.value)} value={username} />
         <input type="text" placeholder='Enter room id' onChange={(e)=>setRoom(e.target.value)} value={room}/>
